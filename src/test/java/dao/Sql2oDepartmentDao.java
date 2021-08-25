56a053eae267abb155c0c99bf5fc657cb307764e
Tevin-creator <isaactevin67@gmail..com>
2021-08-25 16:09:04 +0300

    added tests in dao file

--- src/test/java/dao/Sql2oDepartmentDao.java
+++ src/test/java/dao/Sql2oDepartmentDao.java
@@ -1,19 +1,112 @@
 package dao;
 
-import models.User;
+import models.Department;
+import models.News;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-import java.util.List;
+import java.util.Arrays;
 
-public interface UserDao {
-    //create
-    void add(User user);
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-    //read
-    List<User> getAll();
-    User findById(int id);
-    List<User> getAllUsersByDepartment(int departmentid);
+public class Sql2oDepartmentDaoTest {
+    private Connection conn;
+    private Sql2oDepartmentDao departmentDao;
+    private Sql2oNewsDao newsDao;
+    private Sql2oUserDao userDao;
 
-    //delete
-    void deleteById(int id);
-    void clearAll();
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao= new Sql2oNewsDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void addingDepartmentSetsId() throws Exception {
+        Department testDepartment = setupDepartment();
+        assertNotEquals(0, testDepartment.getId());
+    }
+
+    @Test
+    public void addedDepartmentsAreReturnedFromGetAll() throws Exception {
+        Department testDepartment = setupDepartment();
+        assertEquals(1, departmentDao.getAll().size());
+    }
+
+    @Test
+    public void noDepartmentsReturnsEmptyList() throws Exception {
+        assertEquals(0, departmentDao.getAll().size());
+    }
+
+    @Test
+    public void findByIdReturnsCorrectDepartment() throws Exception {
+        Department testDepartment = setupDepartment();
+        Department otherDepartment = setupDepartment();
+        assertEquals(testDepartment, departmentDao.findById(testDepartment.getId()));
+    }
+
+//    @Test
+//    public void updateCorrectlyUpdatesAllFields() throws Exception {
+//        Department testDepartment = setupDepartment();
+//        departmentDao.update(testDepartment.getId(), "a", "b", 4);
+//        Department foundDepartment = departmentDao.findById(testDepartment.getId());
+//        assertEquals("a", foundDepartment.getDepartmentName());
+//        assertEquals("b", foundDepartment.getDepartmentDescription());
+//        assertEquals(4, foundDepartment.getTotalEmployees());
+//    }
+
+    @Test
+    public void deleteByIdDeletesCorrectDepartment() throws Exception {
+        Department testDepartment = setupDepartment();
+        Department otherDepartment = setupDepartment();
+        departmentDao.deleteById(testDepartment.getId());
+        assertEquals(1, departmentDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll() throws Exception {
+        Department testDepartment = setupDepartment();
+        Department otherDepartment = setupDepartment();
+        departmentDao.clearAll();
+        assertEquals(0, departmentDao.getAll().size());
+    }
+
+    @Test
+    public void DepartmentReturnsNewsCorrectly() throws Exception {
+        News testNews = new News("BMW", "Electric car launch date revealed", 3);
+        newsDao.add(testNews);
+
+        News otherNews = new News("Agriculture", "New plant vaccine discovered", 5);
+        newsDao.add(otherNews);
+
+        Department testDepartment = setupDepartment();
+        departmentDao.add(testDepartment);
+        departmentDao.addDepartmentToNews(testDepartment, testNews);
+        departmentDao.addDepartmentToNews(testDepartment, otherNews);
+
+        News[] news = {testNews, otherNews};
+        assertEquals(Arrays.asList(news), departmentDao.getAllNewsForDepartments(testDepartment.getId()));
+    }
+
+    //helpers
+    public Department setupDepartment() {
+        Department department = new Department("Finance","Maintain Financial Records",13);
+        departmentDao.add(department);
+        return department;
+    }
+
+
 }
--- src/test/java/dao/Sql2oUserDaoTest.java
+++ src/test/java/dao/Sql2oUserDaoTest.java
@@ -1,4 +1,93 @@
 package dao;
 
+import models.Department;
+import models.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.assertEquals;
+
 public class Sql2oUserDaoTest {
+   private Connection conn;
+   private Sql2oUserDao userDao;
+   private Sql2oDepartmentDao departmentDao;
+   private Sql2oNewsDao newsDao;
+
+   @Before
+   public void setUp() throws Exception {
+      String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+      Sql2o sql2o = new Sql2o(connectionString, "", "");
+      userDao = new Sql2oUserDao(sql2o);
+      departmentDao = new Sql2oDepartmentDao(sql2o);
+      newsDao = new Sql2oNewsDao(sql2o);
+      conn = sql2o.open();
+   }
+
+   @After
+   public void tearDown() throws Exception {
+      conn.close();
+   }
+
+   @Test
+   public void addingUserSetsId() throws Exception {
+      User testUser = setupUser();
+      assertEquals(1, testUser.getId());
+   }
+
+   @Test
+   public void getAll() throws Exception {
+      User user1 = setupUser();
+      User user2 = setupUser();
+      assertEquals(2,userDao.getAll().size());
+   }
+
+   @Test
+   public void getAllUsersByDepartment() throws Exception {
+      Department testDepartment = setupDepartment();
+      Department otherDepartment = setupDepartment();
+
+      User user1 = setupUserForDepartment(testDepartment);
+      User user2 = setupUserForDepartment(testDepartment);
+      User userForOtherDepartment = setupUserForDepartment(otherDepartment);
+      assertEquals(3, userDao.getAllUsersByDepartment(testDepartment.getId()).size());
+   }
+
+   @Test
+   public void deleteById() throws Exception {
+      User testUser = setupUser();
+      User otherUser = setupUser();
+      assertEquals(2, userDao.getAll().size());
+      userDao.deleteById(testUser.getId());
+      assertEquals(1, userDao.getAll().size());
+   }
+
+   @Test
+   public void clearAll() throws Exception {
+      User testUser = setupUser();
+      User otherUser = setupUser();
+      userDao.clearAll();
+      assertEquals(0, userDao.getAll().size());
+   }
+
+   //helpers
+   public User setupUser() {
+      User user = new User("Gideon", 1, "Maintains System");
+      userDao.add(user);
+      return user;
+   }
+
+   public User setupUserForDepartment(Department otherDepartment) {
+      User user = new User("Gideon", 1, "Maintains system");
+      userDao.add(user);
+      return user;
+   }
+
+   public Department setupDepartment() {
+      Department department = new Department("Finance", "Maintains Financial Records", 13);
+      departmentDao.add(department);
+      return department;
+   }
 }
