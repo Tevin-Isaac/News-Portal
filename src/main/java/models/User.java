66ef46b53c15ca233ba422f6859e9c6e45a2600a
Tevin-creator <isaactevin67@gmail..com>
2021-08-25 15:34:56 +0300

    added constructors in User class in User.java

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,46 @@
 package models;
 
 public class User {
+    private String username;
+    private int departmentid;
+    private String role;
+    private int id;
+
+    public User(String username, int departmentid, String role) {
+        this.username = username;
+        this.departmentid = departmentid;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getDepartmentId() {
+        return departmentid;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setDepartmentId(int departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
