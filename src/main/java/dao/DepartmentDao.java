d494c97e4ea2c3e6102f002c843f79fe4fc729f3
Tevin-creator <isaactevin67@gmail..com>
2021-08-25 15:46:09 +0300

    imported news class and added constructors in NewsDao.java

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,29 @@
 package dao;
 
+import models.Department;
+import models.News;
+import models.User;
+
+import java.util.List;
+
 public interface DepartmentDao {
+
+    //create
+    void add(Department department);
+    void addDepartmentToNews(Department department, News news);
+
+    //read
+    List<Department> getAll();
+    Department findById(int id);
+    List<User> getAllUsers(int departmentid);
+    List<News> getAllNewsForDepartments(int departmentid);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
+
+
+    //update
+//    void update(int id, String name, String description, int totalemployees);
 }
